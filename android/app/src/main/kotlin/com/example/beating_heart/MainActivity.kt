cb08e86e85176cab30b1f0b0c96c283ca0c8904f
farmanullahQAU <farmanullahk437@gmail.com>
2023-08-19 19:38:24 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/beating_heart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.beating_heart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a blue toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const BeatingHeart());
+  }
+}
+
+class BeatingHeart extends StatefulWidget {
+  const BeatingHeart({super.key});
+
+  @override
+  State<BeatingHeart> createState() => _BeatingHeartState();
+}
+
+class _BeatingHeartState extends State<BeatingHeart>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500))
+      ..repeat(reverse: true);
+
+    _animation = Tween(begin: 1.0, end: 1.2).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Beating Heart"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Code Symmetry",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            AnimatedBuilder(
+                animation: _animationController,
+                builder: (context, widget) {
+                  return Transform.scale(
+                    scale: _animation.value,
+                    child: const Icon(
+                      Icons.favorite,
+                      size: 100,
+                      color: Colors.red,
+                    ),
+                  );
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
